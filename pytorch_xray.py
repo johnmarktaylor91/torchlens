9de01fe89f61488985e1e2f8adaf1b7c3384780a
JohnMark Taylor <johnmarktaylor@g.harvard.edu>
2022-10-16 16:07:43 -0400

    Working on the pytorch-xray model.

--- pytorch_xray.py
+++ pytorch_xray.py
@@ -1,15 +1,333 @@
 import copy
 import random
 from collections import OrderedDict
-from typing import Union, List
+from sys import getsizeof
+from typing import Callable, Dict, List, Tuple, Union
 
 import torch
 from torch import nn
 from torch import Tensor
 
 
-def make_barcode():
+def make_barcode() -> int:
+    """Generates a random integer hash for a layer to use as internal label (invisible from user side).
+
+    Returns:
+        Random hash.
+    """
     return random.getrandbits(128)
+
+
+def get_tensor_memory_amount(t: torch.Tensor) -> int:
+    """Returns the size of a tensor in bytes.
+
+    Args:
+        t: Tensor.
+
+    Returns:
+        Size of tensor in bytes.
+    """
+    return getsizeof(t.storage())
+
+
+def human_readable_size(size: int, decimal_places: int = 2) -> str:
+    """Utility function to convert a size in bytes to a human-readable format.
+
+    Args:
+        size: Number of bytes.
+        decimal_places: Number of decimal places to use.
+
+    Returns:
+        String with human-readable size.
+    """
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
+        if size < 1024.0 or unit == 'PB':
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f} {unit}"
+
+
+def readable_tensor_size(t: torch.Tensor) -> str:
+    """Returns the size of a tensor in human-readable format.
+
+    Args:
+        t: Tensor.
+
+    Returns:
+        Human-readable size of tensor.
+    """
+    return human_readable_size(get_tensor_memory_amount(t))
+
+
+def get_module_from_address(model: nn.Module, address: str) -> nn.Module:
+    """Given a model and an address to a layer, returns the module at that address.
+    The address gives nested instructions for going from the top-level module to the desired module,
+    in the format 'level1.level2.level3', etc. If a level is a string, then it'll be
+    indexed by looking it up as an attribute; if an integer greater than or equal to zero,
+    it'll be indexed like a list. For example, 'classifier.1' will first grab
+    the 'classifier' attribute of the network, and then go into the second element of that list.
+
+    Args:
+        model: PyTorch model.
+        address: String address.
+
+    Returns:
+        The module at the given address.
+    """
+    address = address.split('.')
+    module = model
+    for a in address:
+        if (a.isdigit()) and (int(a) >= 0):
+            module = module[int(a)]
+        else:
+            module = getattr(module, a)
+    return module
+
+
+def get_bottom_level_modules(model: nn.Module) -> Dict[str, nn.Module]:
+    """Recursively crawls through a given model, and returns a dict of the bottom-level
+    modules, with keys corresponding to their address, and values corresponding to the
+    modules themselves.
+
+    Args:
+        model: PyTorch model.
+
+    Returns:
+        Dict of bottom-level modules.
+    """
+    module_stack = [('', model)]
+    module_dict = OrderedDict()
+    while len(module_stack) > 0:
+        module_tuple = module_stack.pop(0)
+        module_address, module = module_tuple
+        module_children = list(module.named_children())
+        if len(module_children) == 0:
+            module_dict[module_address] = module
+        else:
+            children_to_add = []
+            for name, child in module_children:
+                child_address = f"{module_address}.{name}" if module_address else name
+                children_to_add.append((child_address, child))
+            module_stack = children_to_add + module_stack
+    return module_dict
+
+
+def get_all_submodules(model: nn.Module) -> List[nn.Module]:
+    """Recursively gets list of all submodules for given module, no matter their level in the
+    hierarchy; this includes the model itself.
+
+    Args:
+        model: PyTorch model.
+
+    Returns:
+        List of all submodules.
+    """
+    submodules = [model]
+    for module in model.children():
+        submodules.append(module)
+        submodules += get_all_submodules(module)
+    return submodules
+
+
+def hook_bottom_level_modules(model: nn.Module,
+                              hook_fn: Callable,
+                              hook_handles: List) -> List:
+    """Hook all bottom-level modules in a model with given function and return
+    list of hook handles (so as to easily clear later).
+
+    Args:
+        model: PyTorch model.
+        hook_fn: Hook function.
+        hook_handles: Pre-allocated list for storing the hook handles.
+
+    Returns:
+        List of tuples (module_pointer, hook_handle) for each module.
+    """
+    bottom_level_modules = get_bottom_level_modules(model)
+    for module_address, module in bottom_level_modules.items():
+        hook_handle = module.register_forward_hook(hook_fn)
+        hook_handles.append(hook_handle)
+    return hook_handles
+
+
+def clear_hooks(hook_handles: List):
+    """Takes in a list of tuples (module, hook_handle), and clears the hook at that
+    handle for each module.
+
+    Args:
+        hook_handles: List of tuples (module, hook_handle)
+
+    Returns:
+        Nothing.
+    """
+    for hook_handle in hook_handles:
+        hook_handle.remove()
+
+
+def clear_model_keyword_attributes(model: nn.Module, attribute_keyword: str = 'xray'):
+    """Recursively clears the given attribute from all modules in the model.
+
+    Args:
+        model: PyTorch model.
+        attribute_keyword: Any attribute with this keyword will be cleared.
+
+    Returns:
+        Nothing.
+    """
+    for module in get_all_submodules(model):
+        for attribute_name in dir(module):
+            if attribute_keyword in attribute_name:
+                delattr(module, attribute_name)
+
+
+def cleanup_model(model: nn.Module, hook_handles: List) -> nn.Module:
+    """Reverses all temporary changes to the model (namely, the forward hooks and added
+    model attributes) that were added for PyTorch x-ray (scout's honor; leave no trace).
+
+    Args:
+        model: PyTorch model.
+        hook_handles: List of hooks.
+
+    Returns:
+        Original version of the model.
+    """
+    clear_hooks(hook_handles)
+    clear_model_keyword_attributes(model, attribute_keyword='xray')
+    return model
+
+
+def prepare_model(model: nn.Module,
+                  hook_handles: List,
+                  mode: str = 'modules_only',
+                  which_layers: Union[str, List] = 'all') -> List:
+    """Prepares the model to return its inner layer activations.
+
+    Args:
+        model: Model to prepare.
+        hook_handles: Pre-allocated list to store the hooks.
+        mode: Either 'modules_only' for just the modules, or 'exhaustive' for all function calls.
+        which_layers: List of layers to include.
+
+    Returns:
+        Model with hooks and attributes added.
+    """
+    if mode not in ['modules_only', 'exhaustive']:
+        raise ValueError("Mode must be either 'modules_only' or 'exhaustive'.")
+
+    bottom_level_modules = get_bottom_level_modules(model)
+    xray_all_modules_dict = OrderedDict({'which_layers': which_layers,
+                                         'mode': mode})
+    for module_address, module in bottom_level_modules.items():
+        setattr(module, f'xray_this_module_dict', OrderedDict())  # Info for just this module.
+        setattr(module, f'xray_all_modules_dict',
+                xray_all_modules_dict)  # Pointer to dictionary of info for whole model.
+    hook_handles = hook_bottom_level_modules(model,
+                                             hook_fn=record_module_forward_pass,
+                                             hook_handles=hook_handles)
+    return hook_handles
+
+
+def xray_model(model: nn.Module,
+               x: torch.Tensor,
+               mode: str = 'modules_only',
+               which_layers: Union[str, List] = 'all',
+               visualize_opt: str = 'none') -> OrderedDict[str, OrderedDict]:
+    """Run a forward pass through a model, and return activations of desired hidden layers.
+    Specify mode as 'modules_only' to do so only for proper modules, or as 'exhaustive' to
+    also return activations from non-module functions. If only a subset of layers
+    is desired, specify the list of layer names (e.g., 'conv1_5') in which_layers; if you wish to
+    further specify that only certain passes through a layer should be saved
+    (i.e., in a recurrent network, only save the third pass through a layer), then
+    add :{pass_number} to the layer name (e.g., 'conv1_5:3'). Additionally, the graph
+    can be visualized if desire to see the architecture and easily reference the names.
+
+    Args:
+        model: PyTorch model
+        x: desired Tensor input.
+        mode: 'modules_only' to return activations only for module objects, or
+            'exhaustive' to do it for ALL tensor operations.
+        which_layers: List of layers to include. If 'all', then include all layers.
+        visualize_opt: Whether, and how, to visualize the network; 'none' for
+            no visualization, 'rolled' to show the graph in rolled-up format (i.e.,
+            one node per layer if a recurrent network), or 'unrolled' to show the graph
+            in unrolled format (i.e., one node per pass through a layer if a recurrent)
+
+    Returns:
+        activations: Dict of dicts with the activations from each layer.
+    """
+    if mode not in ['modules_only', 'exhaustive']:
+        raise ValueError("Mode must be either 'modules_only' or 'exhaustive'.")
+    if visualize_opt not in ['none', 'rolled', 'unrolled']:
+        raise ValueError("Visualization option must be either 'none', 'rolled', or 'unrolled'.")
+
+    hook_handles = []
+    HistoryTensor.clear_history()
+    x_history = HistoryTensor(x, mode, which_layers)
+
+    # Wrap everything in a try-except block to guarantee the model remains unchanged in case of error.
+    try:
+        hook_handles = prepare_model(model, hook_handles)
+        output = model(x_history)
+        module_output_dict = model.xray_all_modules_dict
+        module_output_dict = postprocess_module_output_dict(module_output_dict)
+        HistoryTensor.postprocess_tensor_history()
+        tensor_history = HistoryTensor.tidy_tensor_history
+        if visualize_opt != 'none':
+            model_graph = make_model_graph(module_output_dict,
+                                           tensor_history,
+                                           mode,
+                                           visualize_opt)
+            render_model_graph(model_graph)
+        cleanup_model(model, hook_handles)
+        if mode == 'modules_only':
+            return module_output_dict
+        elif mode == 'exhaustive':
+            return tensor_history
+    except Exception as e:  # if anything fails, clean up the model and re-raise the error.
+        print("Execution failed somewhere, returning model to original state...")
+        cleanup_model(model, hook_handles)
+        HistoryTensor.clear_history()
+        raise e
+
+
+def show_model_graph(model: nn.Module,
+                     x: torch.Tensor,
+                     mode: str = 'modules_only',
+                     visualize_opt: str = 'rolled') -> None:
+    """Visualize the model graph.
+
+    Args:
+        model: PyTorch model.
+        x: Input for which you want to visualize the graph (this is needed in case the graph varies based on input)
+        mode: 'modules_only' to only view modules, 'exhaustive' to
+            view all tensor operations.
+        visualize_opt: 'rolled' to show the graph in rolled-up format (one node
+            per layer, even if multiple passes), or 'unrolled' to view with
+            one node per operation.
+
+    Returns:
+        Nothing.
+    """
+    if mode not in ['modules_only', 'exhaustive']:
+        raise ValueError("Mode must be either 'modules_only' or 'exhaustive'.")
+    if visualize_opt not in ['none', 'rolled', 'unrolled']:
+        raise ValueError("Visualization option must be either 'none', 'rolled', or 'unrolled'.")
+
+    hook_handles = []
+    try:
+        hook_handles = prepare_model(model, hook_handles)
+        module_output_dict = model.xray_all_modules_dict
+        module_output_dict = postprocess_module_output_dict(module_output_dict)
+        model_graph = make_model_graph(module_output_dict,
+                                       tensor_history=None,
+                                       mode=mode,
+                                       visualize_opt=visualize_opt)
+        render_model_graph(model_graph)
+        cleanup_model(model, hook_handles)
+    except Exception as e:  # if anything fails, clean up the model and re-raise the error.
+        print("Execution failed somewhere, returning model to original state...")
+        cleanup_model(model, hook_handles)
+        raise e
 
 
 class HistoryTensor(torch.Tensor):
@@ -369,31 +687,3 @@ def make_model_transparent(model: nn.Module):
     """
     transparent_model = TransparentModel(model)
     return transparent_model
-
-
-def get_model_activations(model: nn.Module,
-                          x: torch.Tensor,
-                          mode: str = 'modules',
-                          which_layers: Union[str, List] = 'all'):
-    """Run a forward pass through a model, and return activations of desired hidden layers.
-    Specify mode as 'modules' to do so only for PyTorch modules, or as 'functions' to
-    also return activations from non-module functions. If only a subset of layers
-    are desired, specify the list of layer names (e.g., 'conv1_5') in which_layers; if you wish to
-    further specify that only certain passes through a layer should be saved
-    (i.e., in a recurrent network, only save the third pass through a layer), then
-    add :{pass_number} to the layer name (e.g., 'conv1_5:3').
-
-    Args:
-        model: PyTorch model
-        x: desired Tensor input.
-        mode: 'modules' or 'functions'
-        which_layers: List of layers to include. If 'all', then include all layers.
-
-    Returns:
-        activations: Dict of activations.
-    """
-    model_transparent = make_model_transparent(model)
-    x_hist = HistoryTensor(x, mode, which_layers)
-    output, layer_activations = model_transparent(x_hist)
-    model_orig = return_model_to_normal(model)
-    return layer_activations
