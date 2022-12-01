b5f39351b3426f2e18c5939ff98a2272c2637422
JohnMark Taylor <johnmarktaylor@g.harvard.edu>
2022-12-01 12:34:11 -0500

    Tweaked graph visuals

--- torchlens/vis.py
+++ torchlens/vis.py
@@ -148,12 +148,12 @@ def add_rolled_edges_for_node(node: Dict,
         # Annotate passes for the child and parent nodes for the edge only if they vary across passes.
 
         if node['edges_vary_across_passes']:
-            tail_label = f"out: {int_list_to_compact_str(parent_pass_nums)}"
+            tail_label = f"  out\n{int_list_to_compact_str(parent_pass_nums)}  "
         else:
             tail_label = ''
 
         if child_node['edges_vary_across_passes'] and not child_node['is_model_output']:
-            head_label = f"in: {int_list_to_compact_str(child_pass_nums)}"
+            head_label = f"  in\n{int_list_to_compact_str(child_pass_nums)}  "
         else:
             head_label = ''
 
