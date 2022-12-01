2acf8ee1ca156111620846d713f81eff8ab2d7ac
JohnMark Taylor <johnmarktaylor@g.harvard.edu>
2022-12-01 10:35:56 -0500

    Made setup.py file; goal is to make it pip installable, changed license to GNU instead of MIT.

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "numpy",
+    "pandas",
+    "torch",
+    "torchvision",
+    "tqdm",
+    "ipython",
+    "graphviz"
+]
+
+setup(
+    name='torchlens',
+    version='0.1.0',
+    description="A package for extracting and understanding PyTorch model activations with minimal code",
+    long_description=readme,
+    author="JohnMark Taylor",
+    author_email='johnmarkedwardtaylor@gmail.com',
+    url='https://github.com/johnmarktaylor91/torchlens',
+    packages=['torchlens'],
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU GPL v3",
+    zip_safe=False,
+    keywords='torch torchlens features',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU GPL v3',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
