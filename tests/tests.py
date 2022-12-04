4acc1d5161d6f0f6f777e4b83384adb9cfb1bd08
JohnMark Taylor <johnmarktaylor@g.harvard.edu>
2022-12-04 09:19:26 -0500

    Cleaned up the repository

--- tests/tests.py
+++
@@ -1,4 +0,0 @@
-"""Unit testing script. The unit tests include:
-- Does the package leave the network unchanged, and does it leave the torch namespace unchanged?
-- Does it correctly reconstruct the inputs for a representative sampling of different networks?
-"""
