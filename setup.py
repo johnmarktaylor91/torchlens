2a258828a51eb655394af855daa0c7193a92fd12
JohnMark Taylor <johnmarktaylor@g.harvard.edu>
2024-08-22 16:50:46 -0700

    Fixed a bug where TorchLens wasn't accounting for the dim argument in torch.max and torch.min (they return a tuple subclass with the values and the indices).

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name="torchlens",
-    version="0.1.17",
+    version="0.1.18",
     description="A package for extracting activations from PyTorch models",
     long_description="A package for extracting activations from PyTorch models. Contains functionality for "
                      "extracting model activations, visualizing a model's computational graph, and "
