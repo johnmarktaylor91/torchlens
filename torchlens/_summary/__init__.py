84b512263069c44951f2cc95dd278363cb9f0de9
JohnMark Taylor <johnmarkedwardtaylor@gmail.com>
2026-07-05 12:42:35 -0400

    chore: delete dead private modules

--- torchlens/_summary/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Private compatibility shim for moved summary internals."""
-
-from ..visualization._summary_internal import (
-    format_discoverability_summary,
-    format_model_repr,
-    render_model_summary,
-)
-
-__all__ = ["format_discoverability_summary", "format_model_repr", "render_model_summary"]
